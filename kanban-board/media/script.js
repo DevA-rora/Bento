073bf64d6ab965689b152edf50c6d7cb77e7637d
Dev Arora <dev.arora@stu.burgmann.act.edu.au>
2026-06-07 22:32:15 +1000

    render cards function

--- kanban-board/media/script.js
+++ kanban-board/media/script.js
@@ -1,9 +1,32 @@
 // acquire the VSCode API:
 const vscode = acquireVsCodeApi();
 
+function renderCards(cards) {
+    cards.forEach((card) => {
+
+    // put the card on screen:
+    const cardEl = document.createElement('div');
+
+    // put the title text in the card
+    cardEl.textContent = card.title;
+
+    // css name so we can style it:
+    cardEl.className = 'card';
+
+    // find the column it belongs to:
+    const columnEl = document.getElementById('column-' + card.column);
+
+    // plug the card into the column. it now appears on screen:
+    columnEl.appendChild(cardEL);
+        
+    })
+}
+
+// listener function
 window.addEventListener('message', (event) => {
     const message = event.data;
     if (message.command == 'init') {
+        renderCards(message.cards)
         console.log('Got cards:', message.cards);
     }
 });
