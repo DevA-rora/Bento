c7bda0e5847d92551fe44ed00a2abfcc8d75463d
Dev Arora <dev.arora@stu.burgmann.act.edu.au>
2026-06-07 22:01:38 +1000

    externalised html, added cards data structure

--- kanban-board/src/extension.ts
+++ kanban-board/src/extension.ts
@@ -1,6 +1,24 @@
 // disposables are little objects that keep track of things.
+// every app is the data + things you can do to it.
 
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'; // VSCode extensions API
+import * as fs from 'fs'; // file system API (lets us read files)
+import * as path from 'path'; // path API (lets us build paths)
+
+// build the card object (I can modify this later)
+interface Card {
+	id: number;
+	title: string;
+	description?: string;
+	column: string;
+}
+
+let cards: Card[] = [
+	{ id: 1, title: "Fix Bug", description: "something lol", column: "todo"},
+	{ id: 2, title: "Fix Bug", description: "another something lol", column: "doing"},
+	{ id: 3, title: "Fix Bug", column: "doing"} // don't need a description because of the "?"
+]
+
 
 // this is like "main" in python.
 export function activate(context: vscode.ExtensionContext) {
@@ -15,28 +33,27 @@ export function activate(context: vscode.ExtensionContext) {
 			'kanbanBoard.view', // 1. viewType: unique string ID
 			'Kanban Board', // 2. title: what shows on the tag when open in the editor
 			vscode.ViewColumn.One, // means that the webview opens in the MAIN editor area.
-			{} // 4. options: leave as {} for now.
+			{
+				enableScripts: true, // enables javascript in the webview. (the JSX code)
+			}
 		);
 
-		panel.webview.html = `
-		<!DOCTYPE html>
-		<html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<title>Kanban Board</title>
-		</head>
-		<body>
-			<h1>Kanban Board</h1>
-		</body>
-		</html>
-		`
+		const htmlPath = path.join(context.extensionPath, 'media', 'board.html'); // path to board.html
+		panel.webview.html = fs.readFileSync(htmlPath, 'utf8'); // read the file and convert it to a string to pass as the webview.
 		
-		// lets see if this works!
+		// listener function:		
+		panel.webview.onDidReceiveMessage((message) => {
+			console.log('Message from webview:', message);
+			if (message.command === 'buttonClicked') {
+				vscode.window.showInformationMessage('Button clicked!');
+			}
+		});
 
 	});
 
+
 	context.subscriptions.push(boardDisposable);
+
 }
 
 // run "main"
